560b61f723d84e86a21cc5e985d759911b4c8144
peyrin <peyrin@berkeley.edu>
2017-10-07 14:19:17 -0700

    http post sends username now

--- app.py
+++ app.py
@@ -12,13 +12,16 @@ db = client['schedule']
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/<name>/insert', methods=['POST', 'GET'])
-def insert(name):
+@app.route('/insert', methods=['POST', 'GET'])
+def insert():
     if request.method == 'POST':
+        name = request.form['username']
+        date = request.form['date']
         start_time = int(request.form(['start time']))
         end_time = int(request.form(['end time']))
         event = request.form(['event name'])
-        post = {"start": start_time,
+        post = {"date": date,
+                "start": start_time,
                 "end": end_time,
                 "event": event}
         inserted_event = db[name].insert_one(post)
